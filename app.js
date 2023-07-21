ee4c8de42ee90a42623d9c98a3a0392839682fc8
EC2 Default User <ec2-user@ip-172-31-24-225.ap-northeast-1.compute.internal>
2023-07-21 07:44:14 +0000

    thisとchildren

--- app.js
+++ app.js
@@ -1,27 +1,41 @@
+// // // // $(function(){
+// // // //   $('.box1').slideDown();
+// // // // });
+
+// // // // $(function(){
+// // // //   $('.box1').hide();
+// // // //   // $('.box1').css({'background-color': '#0000ff'});
+// // // // });
+
 // // // $(function(){
-// // //   $('.box1').slideDown();
+// // //   $('.box1').slideDown(2000,function(){
+// // //     $('.box1').css({
+// // //       'background-color': '#0000ff',
+// // //       'width': '200px',
+// // //       'height': '100px'
+// // //     }).slideUp(1000);
+// // //   });
 // // // });
 
+// // イベント処理
 // // $(function(){
-// //   $('.box1').hide();
-// //   // $('.box1').css({'background-color': '#0000ff'});
+// //   $('.box1').on('click',function(){
+// //     $('.box1').addClass('box1-ext');
+// //   });
+// //   $('.box1').mouseout(function(){
+// //     $('.box1').removeClass('box1-ext');
+// //   });
 // // });
 
+// thisとchildren
 // $(function(){
-//   $('.box1').slideDown(2000,function(){
-//     $('.box1').css({
-//       'background-color': '#0000ff',
-//       'width': '200px',
-//       'height': '100px'
-//     }).slideUp(1000);
+//   $('.box1').on('click',function(){
+//     $(this).slideUp();
 //   });
 // });
 
 $(function(){
-  $('.box1').on('click',function(){
-    $('.box1').addClass('box1-ext');
-  });
-  $('.box1').mouseout(function(){
-    $('.box1').removeClass('box1-ext');
+  $('button').on('click',function(){
+    $('ul').children().css('color','red');
   });
 });
