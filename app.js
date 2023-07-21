97248a582d7dcd06ea145ad3cf29b5d37f9ed721
EC2 Default User <ec2-user@ip-172-31-24-225.ap-northeast-1.compute.internal>
2023-07-21 07:24:07 +0000

    jQuery_add

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+// // // $(function(){
+// // //   $('.box1').slideDown();
+// // // });
+
 // // $(function(){
-// //   $('.box1').slideDown();
+// //   $('.box1').hide();
+// //   // $('.box1').css({'background-color': '#0000ff'});
 // // });
 
 // $(function(){
-//   $('.box1').hide();
-//   // $('.box1').css({'background-color': '#0000ff'});
+//   $('.box1').slideDown(2000,function(){
+//     $('.box1').css({
+//       'background-color': '#0000ff',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp(1000);
+//   });
 // });
 
 $(function(){
-  $('.box1').slideDown(2000,function(){
-    $('.box1').css({
-      'background-color': '#0000ff',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp(1000);
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
